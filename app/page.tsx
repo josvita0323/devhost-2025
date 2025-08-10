1a19d6c11a6bc6400d8938d6ee2cb3ec912bea6c
Yash <yashlaxman@gmail.com>
2025-08-10 18:03:52 +0530

    add timeline section

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,14 @@
 import Hero from "@/components/Hero";
-import Timeline from "@/components/Timeline";
 import AboutDevhost from "@/components/AboutDevhost";
-import { Flame, MicVocal } from "lucide-react";
 import { Events } from "@/components/Events";
-import Image from "next/image";
+import TimelineSection from "@/components/TimeLine";
 
 export default function Home() {
-  const markers = [
-    {
-      time: "09:30",
-      icon: <MicVocal className="h-auto sm:w-8" />,
-      title: "Masterclass",
-      description: "Stuff",
-    },
-    {
-      time: "09:30",
-      icon: <Flame className="h-auto sm:w-8" />,
-      title: "Masterclass",
-      description: "Stuff",
-    },
-    {
-      time: "09:15",
-      icon: <Flame className="h-auto sm:w-8" />,
-      title: "Masterclass",
-      description: "Stuff",
-    },
-  ];
   return (
     <div>
       <Hero />
       <AboutDevhost />
-      <Timeline startHour={8} endHour={18} markers={markers} />
+      <TimelineSection />
       <Events />
     </div>
   );
