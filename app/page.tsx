098f5fcb1b158d4a3b6376bc98a139a21b7ca396
josvita0323 <concessao.josvita@gmail.com>
2025-08-15 23:37:02 +0530

    Add AboutHackathon component and merge stash changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
+"use client"
 import Hero from "@/components/Hero";
 import AboutDevhost from "@/components/AboutDevhost";
 import { Events } from "@/components/Events";
 import TimelineSection from "@/components/TimeLine";
+import AboutHackathon from "@/components/AboutHackathon";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <AboutDevhost />
+      <AboutHackathon/>
       <TimelineSection />
       <Events />
     </div>
