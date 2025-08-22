8bc34214768941a14b56f8b0bd479842da1bd5d6
Hitha <hithabadikillaya@gmail.com>
2025-08-23 00:12:59 +0530

    redesigned map

--- components/Map.tsx
+++ components/Map.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { MapPin } from "lucide-react";
 
-const Map = () => {
+const Map = () => { 
   return (
     <section className="relative min-h-screen w-full bg-black text-white flex items-center justify-center overflow-hidden px-4 sm:px-6">
       {/* neon grid */}
